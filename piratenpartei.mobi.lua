8d55c93e77ed815dabb4092f5cd9bd8b5d02549c
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-06-04 22:13:12 +0200

    typo

--- piratenpartei.mobi.lua
+++ piratenpartei.mobi.lua
@@ -2,8 +2,8 @@
 -- Variable _a is replaced with zone name
 
 -- NS records
-slave("c.ns.buddyns.com.", "173.244.206.26")
-slave("c.ns.buddyns.com.", "88.198.106.11")
+slave("a.transfer.buddyns.com.", "173.244.206.26")
+slave("b.transfer.buddyns.com.", "88.198.106.11")
 
 -- A records
 a(_a, "178.250.135.54")
