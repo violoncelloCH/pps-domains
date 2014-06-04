5db7f28df78805ecd06d7be19c21213bc8b767b9
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-06-04 21:55:27 +0200

    add lua for mobi

---
+++ piratenpartei.mobi.lua
@@ -0,0 +1,9 @@
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+
+-- A records
+a(_a, "178.250.135.54")
+
+-- CNAME records
+cname("www", "httponly.piratenpartei.ch")
+
