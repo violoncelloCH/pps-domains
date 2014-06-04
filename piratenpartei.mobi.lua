d85430a2617bf7310cf382bad9dabca64f39bb6a
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-06-04 22:09:15 +0200

    allow AXFR

--- piratenpartei.mobi.lua
+++ piratenpartei.mobi.lua
@@ -2,8 +2,8 @@
 -- Variable _a is replaced with zone name
 
 -- NS records
-slave("c.ns.buddy.ns.com", "173.244.206.26")
-slave("c.ns.buddy.ns.com", "88.198.106.11")
+slave("c.ns.buddy.ns.com.", "173.244.206.26")
+slave("c.ns.buddy.ns.com.", "88.198.106.11")
 
 -- A records
 a(_a, "178.250.135.54")
