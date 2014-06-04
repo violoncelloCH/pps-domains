65b7c9a9110f5777cddfd4445d985adf3897dae2
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-06-04 21:54:02 +0200

    convert .mobi into clones

--- trau-keim-plakat.ch.lua
+++
@@ -1,9 +0,0 @@
--- SOA record is automatically generated
--- Variable _a is replaced with zone name
-
--- A records
-a(_a, "178.250.135.54")
-
--- CNAME records
-cname("www", "httponly.piratenpartei.ch")
-
