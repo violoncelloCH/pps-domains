38e78d9c2bbb751217ad88df7992eb5a297ed0fb
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-06-04 22:07:13 +0200

    allow AXFR

--- piratenpartei.mobi.lua
+++ piratenpartei.mobi.lua
@@ -1,6 +1,10 @@
 -- SOA record is automatically generated
 -- Variable _a is replaced with zone name
 
+-- NS records
+slave("c.ns.buddy.ns.com", "173.244.206.26")
+slave("c.ns.buddy.ns.com", "88.198.106.11")
+
 -- A records
 a(_a, "178.250.135.54")
 
