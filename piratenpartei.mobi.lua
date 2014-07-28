9c9c97a1bf948c588eafe22c8c40359f96006512
Philipp Hug <philipp.hug@piratenpartei.ch>
2014-07-28 23:28:22 +0200

    switch to new ips

--- piratenpartei.mobi.lua
+++ piratenpartei.mobi.lua
@@ -6,7 +6,7 @@ slave("a.transfer.buddyns.com.", "173.244.206.26")
 slave("b.transfer.buddyns.com.", "88.198.106.11")
 
 -- A records
-a(_a, "178.250.135.54")
+a(_a, "95.128.36.136")
 
 -- CNAME records
 cname("www", "httponly.piratenpartei.ch")
