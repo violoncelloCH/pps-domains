c3cf85a584f24ff9c0a6039140261c46110b72f5
Philipp Hug <philipp@hug.cx>
2014-07-28 14:09:01 +0200

    maintenance page

--- piratenpartei.mobi.lua
+++ piratenpartei.mobi.lua
@@ -6,7 +6,7 @@ slave("a.transfer.buddyns.com.", "173.244.206.26")
 slave("b.transfer.buddyns.com.", "88.198.106.11")
 
 -- A records
-a(_a, "178.250.135.54")
+a(_a, "130.211.91.161
 
 -- CNAME records
 cname("www", "httponly.piratenpartei.ch")
