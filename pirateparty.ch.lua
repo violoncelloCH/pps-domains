ac704fce3460ad000fbda52e5269b0b5584a4674
J0WI <J0WI@users.noreply.github.com>
2020-04-29 17:51:05 +0200

    Add IPv6

--- pirateparty.ch.lua
+++ pirateparty.ch.lua
@@ -1,5 +1,10 @@
 txt(_a, "v=spf1 a mx ?all")
 
+aaaa(_a, "2a01:ab20:0:4::43")
+aaaa("www", "2a01:ab20:0:4::43")
+aaaa("archive", "2a01:ab20:0:4::43")
+aaaa("mail", "2a01:ab20:0:4::43")
+aaaa("lists", "2a01:ab20:0:4::43")
 a(_a, "149.126.4.43")
 a("www", "149.126.4.43")
 a("archive", "149.126.4.43")
